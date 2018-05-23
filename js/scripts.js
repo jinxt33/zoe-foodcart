89a699129be4d9702c9546be45b14ff0472c28ba
Zoe Zhou <zoezo85@gmail.com>
2018-05-23 15:40:28 -0700

    it will change the js animation to slideToggle()

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("a.find").click(function() {
-    $(".jumbotron.find").toggle();
+    $(".jumbotron.find").slideToggle();
     $(".jumbotron.contact").hide();
     $(".jumbotron.follow").hide();
   });
   $("a.contact").click(function() {
-    $(".jumbotron.contact").toggle();
+    $(".jumbotron.contact").slideToggle();
     $(".jumbotron.find").hide();
     $(".jumbotron.follow").hide();
   });
   $("a.follow").click(function() {
-    $(".jumbotron.follow").toggle();
+    $(".jumbotron.follow").slideToggle();
     $(".jumbotron.contact").hide();
     $(".jumbotron.find").hide();
   });
