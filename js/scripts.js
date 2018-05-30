a39821e983fa9ae587f47f8ad2d49e468771cb21
Zoe Zhou <zoezo85@gmail.com>
2018-05-29 23:08:02 -0700

    it will replace css hover with js hover per baozi

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $("a.find").click(function() {
     $(".jumbotron.find").slideToggle();
     $(".jumbotron.contact").hide();
@@ -14,13 +15,39 @@ $(document).ready(function() {
     $(".jumbotron.contact").hide();
     $(".jumbotron.find").hide();
   });
-  $("img.twitter").hover(function () {
+
+  $("img.twitter").hover(function() {
     $("p.twitter").fadeToggle();
   });
-  $("img.instagram").hover(function () {
+  $("img.instagram").hover(function() {
     $("p.instagram").fadeToggle();
   });
-  $("img.facebook").hover(function () {
-    $("p.facebook").fadeToggle();    
+  $("img.facebook").hover(function() {
+    $("p.facebook").fadeToggle();
   });
+
+  $("img.bz1").hover(function() {
+    $("p.bz1").fadeToggle();
+  });
+  $("img.bz2").hover(function() {
+    $("p.bz2").fadeToggle();
+  });
+  $("img.bz3").hover(function() {
+    $("p.bz3").fadeToggle();
+  });
+  $("img.bz4").hover(function() {
+    $("p.bz4").fadeToggle();
+  });
+  $("img.bz5").hover(function() {
+    $("p.bz5").fadeToggle();
+  });
+  $("img.bz6").hover(function() {
+    $("p.bz6").fadeToggle();
+  });
+
+  $("form#contact").submit(function(event) {
+    event.preventDefault();
+    $("p.thankyou").show();
+  });
+
 });
