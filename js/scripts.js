51b10a06fa1ca5913df5a815ccee120b3eadfa32
Zoe Zhou <zoezo85@gmail.com>
2018-07-09 20:47:56 -0700

    it will reorder scripts and created a constructor that makes boazi objects, a prototype that summarizes the baozi, an each function that collects checkbox selections into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //BUSINESS LOGIC
 var name;
 
-//constructor for custom bun object
-//protoype for printing out bun selections
+function baoZi(bun, protein, extras, sauce) {
+  this.bunSel = bun;
+  this.proteinSel = protein;
+  this.extrasSel = [extras];
+  this.sauceSel = sauce;
+}
+
+baoZi.prototype.info = function() {
+  return this.bunSel + " bao with " + this.proteinSel + ", " + this.extraSel + " glazed with " + this.sauceSel + " sauce.";
+}
 
 function xNan (v) {
   if ( v == null ) {
@@ -18,8 +26,59 @@ var saucesTotal;
 
 var total;
 
+var extrasArray = [];
+
 //USER INTERFACE LOGIC
 $(document).ready(function() {
+
+  $("form#byo").submit(function(event) {
+    event.preventDefault();
+
+    $("input:checkbox[name=extras]:checked").each(function() {
+      var extrasName = $(this).text();
+      extrasArray.push(extrasName);
+    });
+
+    console.log(extrasArray);
+
+    extrasTotal = parseFloat(xNan($("#mushroom:checked").val())) + parseFloat(xNan($("#veggies:checked").val())) + parseFloat(xNan($("#bamboo:checked").val())) + parseFloat(xNan($("#bokchoy:checked").val())) + parseFloat(xNan($("#celery:checked").val())) + parseFloat(xNan($("#egg:checked").val()));
+    // use .each array to clean up above and replace the nan function in biz logic
+
+    bunsTotal = parseFloat(xNan($("input:radio[name=bun]:checked").val()));
+    proteinTotal = parseFloat(xNan($("input:radio[name=protein]:checked").val()));
+    saucesTotal = parseFloat(xNan($("input:radio[name=sauce]:checked").val()));
+
+    total = bunsTotal + proteinTotal + extrasTotal + saucesTotal;
+
+    $("span#totalvalue").text(total);
+    $(".jumbotron.review").slideDown();
+    $("h3#total").show();
+
+    if (total <= 4.0) {
+      $("h3#hungrytiger").hide();
+      $("h3#respect").hide();
+      $("h3#fee").show();
+    } else if (total >= 5.2) {
+      $("h3#fee").hide();
+      $("h3#respect").hide();
+      $("h3#hungrytiger").show();
+    } else {
+      $("h3#hungrytiger").hide();
+      $("h3#fee").hide();
+      $("h3#respect").show();
+    }
+  });
+
+  $("form#contact").submit(function(event) {
+    event.preventDefault();
+
+    name = $("input#name").val();
+
+    $("span#name").text(name);
+    $("form#contact").slideToggle();
+    $("p.thankyou").show();
+  });
+
   $("img.bz1").hover(function() {
     $("p.bz1").fadeToggle();
   });
@@ -59,46 +118,4 @@ $(document).ready(function() {
     $("p.facebook").fadeToggle();
   });
 
-  $("form#contact").submit(function(event) {
-    event.preventDefault();
-
-    name = $("input#name").val();
-
-    $("span#name").text(name);
-
-    $("form#contact").slideToggle();
-    $("p.thankyou").show();
-  });
-
-  $("form#byo").submit(function(event) {
-    event.preventDefault();
-
-    extrasTotal = parseFloat(xNan($("#mushroom:checked").val())) + parseFloat(xNan($("#veggies:checked").val())) + parseFloat(xNan($("#bamboo:checked").val())) + parseFloat(xNan($("#bokchoy:checked").val())) + parseFloat(xNan($("#celery:checked").val())) + parseFloat(xNan($("#egg:checked").val()));
-    // use .each array to clean up above and replace the nan function in biz logic
-
-    bunsTotal = parseFloat(xNan($("input:radio[name=bun]:checked").val()));
-    proteinTotal = parseFloat(xNan($("input:radio[name=protein]:checked").val()));
-    saucesTotal = parseFloat(xNan($("input:radio[name=sauce]:checked").val()));
-
-    total = bunsTotal + proteinTotal + extrasTotal + saucesTotal;
-
-    $("span#totalvalue").text(total);
-    $(".jumbotron.review").slideDown();
-    $("h3#total").show();
-
-    if (total <= 4.0) {
-      $("h3#hungrytiger").hide();
-      $("h3#respect").hide();
-      $("h3#fee").show();
-    } else if (total >= 5.2) {
-      $("h3#fee").hide();
-      $("h3#respect").hide();
-      $("h3#hungrytiger").show();
-    } else {
-      $("h3#hungrytiger").hide();
-      $("h3#fee").hide();
-      $("h3#respect").show();
-    }
-
-  });
 });
